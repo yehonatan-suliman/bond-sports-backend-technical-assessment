14ed5a8fbdcb7ade20d20f98eca313f8ece51709
Yehoantan Suliman <yehoantan.suliman@agamimp.net>
2026-05-26 19:43:46 +0300

    feat: added url logs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from './common/swagger';
+import { SWAGGER_PATH, setupSwagger } from './common/swagger';
 
 async function bootstrap(): Promise<void> {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
   setupSwagger(app);
@@ -11,6 +13,10 @@ async function bootstrap(): Promise<void> {
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT') ?? 3000;
   await app.listen(port);
+
+  const url = await app.getUrl();
+  logger.log(`API listening on ${url}`);
+  logger.log(`Swagger UI at  ${url}/${SWAGGER_PATH}`);
 }
 
 bootstrap();
